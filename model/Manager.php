b21edfc6ba37986239cb12b3c7f479b19eba1b78
David <contact@sitez-vous.com>
2020-03-10 13:33:04 +0100

    Complément commit précédent (model/Manager.php n'avait pas été sauvegardé)

--- model/Manager.php
+++ model/Manager.php
@@ -30,26 +30,6 @@ abstract class Manager{
 		return $list;
 	}
 
-	public function getUnique($id)
-	{
-		$q = $this->db->prepare('SELECT * FROM $this->table WHERE id = :id');
-		
-		$q->bindValue(":id", (int) $id, PDO::PARAM_INT);
-		
-		$q->execute();
-
-		$q->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, $this->classManaged);
-		$actor = $q->fetch();
-		return $actor;
-	}
-
-	function isExist($id){
-		$q = $this->db->prepare('SELECT id FROM $this->table WHERE id = :id');
-		$q->bindValue(':id',$id,PDO::PARAM_INT);
-		$q->execute();
-		$result = $q->fetch();
-
-		return $result;
-	}
+	
 
 }
